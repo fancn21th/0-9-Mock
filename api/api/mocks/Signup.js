b5f9c1614ee99964878db59b7d62bedd946c219e
fancn <fanyj@yikeer.com>
2017-03-24 18:00:29 +0800

    jwt token added

--- api/api/mocks/Signup.js
+++ api/api/mocks/Signup.js
@@ -1,16 +1,25 @@
 'use strict';
+const jwt = require('jsonwebtoken')
+
+const jwtSecret = 'fsdafdasfadsfdsa/asdfdas'
 
 module.exports = {
     Signup : Signup
 }
 
 function Signup(req, res, next) {
+    console.log(req.swagger.params.user.value.phone)
+
+    const token = jwt.sign({
+        phone: req.swagger.params.user.value.phone
+    }, jwtSecret)
+
     res.json({
         errorcode: 200,
-        errormsg: 'there is an error',
+        errormsg: 'mock/there is an error',
         data:{
             userid: 1,
-            tracenum: 'there is a trace number'
+            tracenum: token
         }
     })
 }
